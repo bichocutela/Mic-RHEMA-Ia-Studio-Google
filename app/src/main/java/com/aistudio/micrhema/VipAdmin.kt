5e50f6387c5e6196468abe050b9e43e37bbcf34a
bichocutela <1.86314089e+08+bichocutela@users.noreply.github.com>
2026-08-16 05:58:47 +0000

    Corrigir import do envio de certificado

--- app/src/main/java/com/aistudio/micrhema/VipAdmin.kt
+++ app/src/main/java/com/aistudio/micrhema/VipAdmin.kt
@@ -1,6 +1,7 @@
 package com.aistudio.micrhema
 
 import android.content.Intent
+import android.widget.Toast
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
