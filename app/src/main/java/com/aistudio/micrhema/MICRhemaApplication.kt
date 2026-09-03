3221ca4daff5d696649b7e440e7d9f2e49c126fd
bicho cutela <haydendanex@gmail.com>
2026-09-03 13:35:12 -0300

    fix(notifications): iniciar agendamentos fora da interface

--- app/src/main/java/com/aistudio/micrhema/MICRhemaApplication.kt
+++ app/src/main/java/com/aistudio/micrhema/MICRhemaApplication.kt
@@ -1,14 +1,11 @@
 package com.aistudio.micrhema
 
 import android.app.Application
-import com.google.firebase.FirebaseApp
-import com.google.firebase.FirebaseOptions
 
 class MICRhemaApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         CrashHandler.init(this)
-        
-        
+        BackgroundNotificationCoordinator.initialize(this)
     }
 }
